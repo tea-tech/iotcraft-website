f3d81a583acf8115d38f9b644a775f84644f1b75
jirisitera <jirik.sitera@gmail.com>
2025-02-22 11:20:18 +0100

    yet another fix to actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://jirisitera.github.io',
-  base: '/github-pages',
+  base: '/iotcraft-website',
 });

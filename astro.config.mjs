586894fc9ffb2a096e1c6b8f594c837f7f6b60ff
jirisitera <jirik.sitera@gmail.com>
2025-02-25 00:57:00 +0100

    added starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), starlight(
+    {
+			title: 'IoTcraft Docs',
+			social: {
+				github: 'https://github.com/tea-tech/website',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'start' },
+					],
+				},
+			],
+    }
+  )],
   site: 'https://jirisitera.github.io',
-  base: '/iotcraft-website',
 });

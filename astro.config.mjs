2948194a220ab9f9a107df2bca5a0123765e4c48
jirisitera <jirik.sitera@gmail.com>
2025-03-04 00:26:51 +0100

    wip new landing page design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,30 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import starlight from '@astrojs/starlight';
 import { ion } from "starlight-ion-theme";
+import starlight from '@astrojs/starlight';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  output : "static",
   site: 'https://iotcraft.teatech.cz',
 	base: '/',
+  vite:{
+    plugins: [
+      tailwindcss()
+    ]
+  },
   integrations: [
-		tailwind(), 
 		starlight({
 			title: 'IoTcraft Docs',
 			favicon: '/favicon.ico',
 			logo: {
         src: './src/images/iotcraft.png',
       },
+			editLink: {
+				baseUrl: 'https://github.com/tea-tech/iotcraft-website/edit/master/',
+			},
 			social: {
 				github: 'https://github.com/tea-tech/iotcraft-website',
+				discord: 'https://discord.teatech.cz',
 			},
 			sidebar: [
 				{

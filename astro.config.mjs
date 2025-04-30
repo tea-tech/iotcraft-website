8ed39480305d3cca1aa79bdf762e6532f7eae56e
jirisitera <jirik.sitera@gmail.com>
2025-04-30 02:50:59 +0200

    upgrades to gesture system

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import {defineConfig} from 'astro/config';
-import {ion} from "starlight-ion-theme";
 import starlight from '@astrojs/starlight';
 import tailwindcss from '@tailwindcss/vite';
 
@@ -16,16 +15,16 @@ export default defineConfig({
             favicon: '/favicon.ico',
             logo: {src: './src/images/iotcraft.png'},
             editLink: {baseUrl: 'https://github.com/tea-tech/iotcraft-website/edit/master/'},
-            social: {github: 'https://github.com/tea-tech/iotcraft-website', discord: 'https://discord.teatech.cz'},
+            social: [
+                {icon: 'github', label: 'GitHub', href: 'https://github.com/tea-tech/iotcraft-website'},
+                {icon: 'discord', label: 'Discord', href: 'https://discord.teatech.cz'}
+            ],
             sidebar: [{
                 label: 'Getting Started',
                 items: [
                     {label: 'First Steps', slug: 'start'},
                 ],
-            }],
-            plugins: [
-                ion()
-            ]
+            }]
         })
     ],
 });
